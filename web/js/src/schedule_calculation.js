6ba6f076053f59b2c4df95ff86d36d2cd200a54e
Ginelle Gaisano <ginelle.gaisano@gmail.com>
2015-10-01 13:34:46 -0400

    Using cis timeline instead

--- web/js/src/schedule_calculation.js
+++ web/js/src/schedule_calculation.js
@@ -11,13 +11,23 @@ Dependencies:
     common.js
 */
 
+function createVisObject(startTime, endTime) {
+    var time = {
+        style: "background-color: " + PRIMARY_COLOUR  + "; color: white;",
+        start: startTime,
+        end: endTime
+    };
+    return time;
+}
+
 /*
-Returns an array of sleep times for Everyman sleep schedule (objects for d3-timeline).
+Returns an array of sleep times for Everyman sleep schedule (objects for ).
 
 Parameters:
     startTime: unix timestamp of when core sleep period (longest nap) should begin.
     [Optional] days: number of days for calculate sleep schedule for. Default is 1.
 */
+
 function calculateEverymanSleepSchedule (startTime, days) {
     if (days === undefined) {
         days = 1;
@@ -27,27 +37,25 @@ function calculateEverymanSleepSchedule (startTime, days) {
     for (i = 0; i < 4 * days; i++) {
         if (i % 4 == 0) {
             // add longest nap
-            times.push({
-                "color":PRIMARY_COLOUR,
-                "starting_time": startTime + convertTimeToMilliseconds(Math.floor(i / 4) * 24, TimeEnum.HOURS),
-                "ending_time": startTime + convertTimeToMilliseconds(Math.floor(i / 4) * 24 + 3, TimeEnum.HOURS)
-            });
+            times.push( createVisObject (
+                startTime + convertTimeToMilliseconds(Math.floor(i / 4) * 24, TimeEnum.HOURS),
+                startTime + convertTimeToMilliseconds(Math.floor(i / 4) * 24 + 3, TimeEnum.HOURS)
+            ));
         } else {
-            times.push({
-                "color":PRIMARY_COLOUR,
-                "starting_time": startTime + convertTimeToMilliseconds(
-                        Math.floor(i / 4) * 24 // number of days
-                        + 3 // longest nap
-                        + 5 * (i % 4), // (i % 4)th nap in the day
-                        TimeEnum.HOURS) +
-                        convertTimeToMilliseconds(((i - 1) % 4)* 20, TimeEnum.MINUTES),
-                "ending_time": startTime +convertTimeToMilliseconds(
-                        Math.floor(i / 4) * 24
-                        + 3
-                        + 5 * (i % 4),
-                        TimeEnum.HOURS) +
-                        convertTimeToMilliseconds((i % 4) * 20, TimeEnum.MINUTES),
-            });
+            times.push( createVisObject(
+                startTime + convertTimeToMilliseconds(
+                    Math.floor(i / 4) * 24 // number of days
+                    + 3 // longest nap
+                    + 5 * (i % 4), // (i % 4)th nap in the day
+                    TimeEnum.HOURS) +
+                    convertTimeToMilliseconds(((i - 1) % 4)* 20, TimeEnum.MINUTES),
+                startTime +convertTimeToMilliseconds(
+                    Math.floor(i / 4) * 24
+                    + 3
+                    + 5 * (i % 4),
+                    TimeEnum.HOURS) +
+                    convertTimeToMilliseconds((i % 4) * 20, TimeEnum.MINUTES)
+            ));
         }
 
     }
@@ -55,7 +63,7 @@ function calculateEverymanSleepSchedule (startTime, days) {
 }
 
 /*
-Returns an array of sleep times for Uberman sleep schedule (objects for d3-timeline).
+Returns an array of sleep times for Uberman sleep schedule (objects for ).
 
 Parameters:
     startTime: unix timestamp of when first nap should begin.
@@ -67,13 +75,12 @@ function calculateUbermanSleepSchedule (startTime, days) {
     }
     var times = [];
     for (i = 0; i < 6 * days; i++) {
-        times.push( {
-            "color":PRIMARY_COLOUR,
-            "starting_time": startTime + convertTimeToMilliseconds(i * 4, TimeEnum.HOURS) +
+        times.push( createVisObject(
+            startTime + convertTimeToMilliseconds(i * 4, TimeEnum.HOURS) +
                 convertTimeToMilliseconds(i * 20, TimeEnum.MINUTES),
-            "ending_time": startTime + convertTimeToMilliseconds(i * 4, TimeEnum.HOURS) +
-                convertTimeToMilliseconds((i + 1) * 20, TimeEnum.MINUTES),
-        });
+            startTime + convertTimeToMilliseconds(i * 4, TimeEnum.HOURS) +
+                convertTimeToMilliseconds((i + 1) * 20, TimeEnum.MINUTES)
+        ));
     }
     return times;
 }
